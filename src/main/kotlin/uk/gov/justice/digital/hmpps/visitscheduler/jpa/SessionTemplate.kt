311dd10a1ab7e6c7fd39d2b59f47bb286d60f854
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2021-10-28 16:30:38 +0100

    vb-291 wip session generation with int tests including injectable clock for consistent date and time testing

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/SessionTemplate.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.jpa
+
+import org.hibernate.Hibernate
+import java.time.LocalDate
+import java.time.LocalTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "SESSION_TEMPLATE")
+data class SessionTemplate(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long = 0,
+
+  @Column(nullable = false)
+  val prisonId: String,
+
+  @Column(nullable = false)
+  val visitRoom: String,
+
+  @Column(nullable = false)
+  val visitType: String,
+
+  @Column(nullable = false)
+  val frequency: String,
+
+  @Column
+  val restrictions: String?,
+
+  @Column(nullable = false)
+  val startTime: LocalTime,
+
+  @Column(nullable = false)
+  val endTime: LocalTime,
+
+  @Column(nullable = false)
+  val startDate: LocalDate,
+
+  @Column()
+  val expiryDate: LocalDate?,
+
+  @Column(nullable = false)
+  val closedCapacity: Int,
+
+  @Column(nullable = false)
+  val openCapacity: Int,
+
+) {
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as SessionTemplate
+
+    return id == other.id
+  }
+
+  override fun hashCode(): Int = 0
+
+  @Override
+  override fun toString(): String {
+    return this::class.simpleName + "(id = $id )"
+  }
+}
