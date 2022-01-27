276ad57747db79040a4f5ffe5f80e991924ea547
Phil <95414611+philip-milne@users.noreply.github.com>
2022-01-27 10:46:15 +0000

    Upgrade gradle, deps and jvm target (#37)
    * Upgrade gradle, deps and jvm target
    
    * duplicate line removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
-  kotlin("plugin.spring") version "1.5.31"
-  kotlin("plugin.jpa") version "1.5.31"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
   idea
 }
 
@@ -10,49 +10,56 @@ configurations {
 }
 
 dependencies {
-  agentDeps("com.microsoft.azure:applicationinsights-agent:3.2.0-BETA.4")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+  // Spring
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
+  // Swagger
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
+
+  // AWS
+
+  // Insights
+  agentDeps("com.microsoft.azure:applicationinsights-agent:3.2.4")
+
+  // DB
   implementation("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.3.0")
-  implementation("com.vladmihalcea:hibernate-types-52:2.12.1")
+  implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
+  runtimeOnly("org.postgresql:postgresql:42.3.1")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.google.code.gson:gson:2.8.8")
+  implementation("com.google.code.gson:gson:2.8.9")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+  // HMPPS Libs
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
-
+  // Test
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
-  testImplementation("org.testcontainers:postgresql:1.16.2")
+  testImplementation("org.testcontainers:postgresql:1.16.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:3.12.4")
+  testImplementation("org.mockito:mockito-inline:4.3.1")
   testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "16"
+      jvmTarget = "17"
     }
   }
 }
