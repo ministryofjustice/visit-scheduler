2632149e9b0fe2b994882ab0664ae327f77cbb89
Phil <philip.milne@digital.justice.gov.uk>
2022-01-27 09:38:24 +0000

    duplicate line removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
   // Spring
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
