7ecf8118c3a4cc426ffc7305e0a42b3381bd87f2
Phil <philip.milne@digital.justice.gov.uk>
2022-03-21 12:17:40 +0000

    re-implement equals hash toString

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/VisitContact.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/VisitContact.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.jpa
 
+import org.hibernate.Hibernate
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -25,4 +26,27 @@ data class VisitContact(
   @JoinColumn(name = "VISIT_ID", updatable = false, insertable = false)
   val visit: Visit,
 
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as VisitContact
+
+    if (id != other.id) return false
+    if (contactName != other.contactName) return false
+    if (contactPhone != other.contactPhone) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = id.hashCode()
+    result = 31 * result + contactName.hashCode()
+    result = 31 * result + contactPhone.hashCode()
+    return result
+  }
+
+  override fun toString(): String {
+    return this::class.simpleName + "(id=$id, contactName=$contactName, contactPhone=$contactPhone)"
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/VisitSupport.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/VisitSupport.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.jpa
 
+import org.hibernate.Hibernate
 import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Embeddable
@@ -31,4 +32,25 @@ data class VisitSupport(
   @JoinColumn(name = "VISIT_ID", updatable = false, insertable = false)
   val visit: Visit,
 
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as VisitSupport
+
+    if (id != other.id) return false
+    if (supportDetails != other.supportDetails) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = id.hashCode()
+    result = 31 * result + (supportDetails?.hashCode() ?: 0)
+    return result
+  }
+
+  override fun toString(): String {
+    return this::class.simpleName + "(id=$id, supportDetails=$supportDetails)"
+  }
+}
