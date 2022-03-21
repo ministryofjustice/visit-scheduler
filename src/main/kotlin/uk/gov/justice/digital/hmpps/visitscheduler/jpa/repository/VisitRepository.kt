800d52de79b549733e10fc3b901d64b45a68b028
Phil <95414611+philip-milne@users.noreply.github.com>
2022-03-21 16:33:57 +0000

    lock on visit delete (#60)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/repository/VisitRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/jpa/repository/VisitRepository.kt
@@ -2,10 +2,16 @@ package uk.gov.justice.digital.hmpps.visitscheduler.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.visitscheduler.jpa.Visit
+import javax.persistence.LockModeType
 
 @Repository
 interface VisitRepository : JpaRepository<Visit, String>, JpaSpecificationExecutor<Visit> {
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  fun deleteAllByIdIn(visitId: List<String>)
+
   fun findByPrisonerId(prisonerId: String): List<Visit>
 }
