856c5bb7fec7b3c24df74b562b5dd38212381374
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2022-03-22 14:11:17 +0000

    VB-596 Refactored migration scripts, and changed table/entities from support to support type, also changed name from AvailableSupport.kt to SupportTypeDto.kt and SupportDto.kt to VisitorSupportDto.kt.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/data/VisitDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/data/VisitDto.kt
@@ -7,29 +7,44 @@ import javax.validation.constraints.NotBlank
 
 @Schema(description = "Visit")
 data class VisitDto(
-  @Schema(description = "Visit id", example = "v9-d7-ed-7u", required = true) val id: String,
-  @Schema(description = "Prisoner Id", example = "AF34567G", required = true) val prisonerId: String,
-  @Schema(description = "Prison Id", example = "MDI", required = true) val prisonId: String,
-  @Schema(description = "Visit Room", example = "A1 L3", required = true) val visitRoom: String,
-  @Schema(description = "Visit Type", example = "STANDARD_SOCIAL", required = true) val visitType: String,
-  @Schema(description = "Visit Type Description", example = "Standard Social", required = true) val visitTypeDescription: String,
-  @Schema(description = "Visit Status", example = "RESERVED", required = true) val visitStatus: String,
-  @Schema(description = "Visit Status Description", example = "Reserved", required = true) val visitStatusDescription: String,
+  @Schema(description = "Visit id", example = "v9-d7-ed-7u", required = true)
+  val id: String,
+  @Schema(description = "Prisoner Id", example = "AF34567G", required = true)
+  val prisonerId: String,
+  @Schema(description = "Prison Id", example = "MDI", required = true)
+  val prisonId: String,
+  @Schema(description = "Visit Room", example = "A1 L3", required = true)
+  val visitRoom: String,
+  @Schema(description = "Visit Type", example = "STANDARD_SOCIAL", required = true)
+  val visitType: String,
+  @Schema(description = "Visit Type Description", example = "Standard Social", required = true)
+  val visitTypeDescription: String,
+  @Schema(description = "Visit Status", example = "RESERVED", required = true)
+  val visitStatus: String,
+  @Schema(description = "Visit Status Description", example = "Reserved", required = true)
+  val visitStatusDescription: String,
   @Schema(
     description = "The date and time of the visit",
     example = "2018-12-01T13:45:00",
     required = true
-  ) @field:NotBlank val startTimestamp: LocalDateTime,
+  ) @field:NotBlank
+  val startTimestamp: LocalDateTime,
   @Schema(
     description = "The finishing date and time of the visit",
     example = "2018-12-01T13:45:00",
     required = true
-  ) @field:NotBlank val endTimestamp: LocalDateTime,
-  @Schema(description = "Visitor Concerns", required = false) val visitorConcerns: String? = null,
-  @Schema(description = "Main Contact associated with the visit", required = false) val mainContact: ContactDto? = null,
-  @Schema(description = "List of visitors associated with the visit", required = false) val visitors: List<VisitorDto> = listOf(),
-  @Schema(description = "List of additional support associated with the visit", required = false) val support: List<VisitorSupportDto> = listOf(),
-  @Schema(description = "Session Id identifying the visit session template", example = "123", required = false) val sessionId: Long? = null,
+  ) @field:NotBlank
+  val endTimestamp: LocalDateTime,
+  @Schema(description = "Visitor Concerns", required = false)
+  val visitorConcerns: String? = null,
+  @Schema(description = "Main Contact associated with the visit", required = false)
+  val mainContact: ContactDto? = null,
+  @Schema(description = "List of visitors associated with the visit", required = false)
+  val visitors: List<VisitorDto> = listOf(),
+  @Schema(description = "List of additional support associated with the visit", required = false)
+  val visitorSupport: List<VisitorSupportDto> = listOf(),
+  @Schema(description = "Session Id identifying the visit session template", example = "123", required = false)
+  val sessionId: Long? = null,
 ) {
 
   constructor(visitEntity: Visit) : this(
@@ -46,7 +61,7 @@ data class VisitDto(
     visitorConcerns = visitEntity.visitorConcerns,
     mainContact = visitEntity.mainContact?.let { ContactDto(it) },
     visitors = visitEntity.visitors.map { VisitorDto(it) },
-    support = visitEntity.support.map { VisitorSupportDto(it) },
+    visitorSupport = visitEntity.support.map { VisitorSupportDto(it) },
     sessionId = visitEntity.sessionTemplateId,
   )
 }
