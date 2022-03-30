f78c959258d71d4bda1b56b1436ef04654d9a35b
Phil <philip.milne@digital.justice.gov.uk>
2022-03-30 10:59:14 +0100

    Update from PR review

--- src/main/resources/db/migration/V1_7__create_visit_restriction_column.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE visit
-    ADD COLUMN visit_restriction   VARCHAR(80)
-;
-
-UPDATE visit SET visit_restriction = 'OPEN';
-
-ALTER TABLE visit ALTER COLUMN visit_restriction SET NOT NULL;
