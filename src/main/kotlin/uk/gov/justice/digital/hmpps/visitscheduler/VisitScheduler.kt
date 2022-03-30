1ab7152eda68d34e3ad8c1d7166b177680f8464f
Phil <95414611+philip-milne@users.noreply.github.com>
2022-03-30 10:35:59 +0100

    Upgrade to 4.1.1 and removed redundant deps (#63)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/VisitScheduler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/VisitScheduler.kt
@@ -4,13 +4,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @SecurityScheme(name = "bearerAuth", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT")
 @EnableScheduling
-@EnableRetry
 class VisitScheduler
 
 fun main(args: Array<String>) {
