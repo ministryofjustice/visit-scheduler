f7cd6ce10a9045f576ef59dd91d7f85085dc12d5
Phil <95414611+philip-milne@users.noreply.github.com>
2022-03-31 13:48:16 +0100

    Upgrade to 4.1.2 (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.2"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   idea
@@ -11,42 +11,28 @@ configurations {
 
 dependencies {
 
-  // Spring
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+  agentDeps("com.microsoft.azure:applicationinsights-agent")
+
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-  // Swagger
   implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.6")
   implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
 
-  // AWS
-
-  // Insights
-  agentDeps("com.microsoft.azure:applicationinsights-agent")
-
-  // DB
-  runtimeOnly("org.postgresql:postgresql:42.3.3")
+  runtimeOnly("org.postgresql:postgresql")
   runtimeOnly("org.flywaydb:flyway-core")
-  implementation("com.vladmihalcea:hibernate-types-52:2.14.1")
-  implementation("javax.transaction:javax.transaction-api:1.3")
-  implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.google.code.gson:gson:2.9.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
 
-  // HMPPS Libs
-
-  // Test
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.32.0")
-  testImplementation("org.testcontainers:postgresql:1.16.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
+  testImplementation("org.testcontainers:postgresql:1.16.3")
 }
 
 java {
