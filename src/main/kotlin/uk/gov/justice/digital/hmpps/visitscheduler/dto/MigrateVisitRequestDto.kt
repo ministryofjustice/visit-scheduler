4619036fb05c77034ce8410e1062b7793f228d03
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2022-05-11 16:01:42 +0100

    VB-786 Fully Support Outcome mapping from NOMIS to VSIP, update swagger doc on outcomestatus

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/MigrateVisitRequestDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/MigrateVisitRequestDto.kt
@@ -27,7 +27,7 @@ data class MigrateVisitRequestDto(
   @Schema(description = "Visit Status", example = "RESERVED", required = true)
   @field:NotNull
   val visitStatus: VisitStatus,
-  @Schema(description = "Outcome Status", example = "NOT_RECORDED", required = false)
+  @Schema(description = "Outcome Status", defaultValue = "NOT_RECORDED", required = false)
   val outcomeStatus: OutcomeStatus? = OutcomeStatus.NOT_RECORDED,
   @Schema(description = "Visit Restriction", example = "OPEN", required = true)
   @field:NotNull
