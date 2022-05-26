ace6e4171a28d8ea087ac01a02f042e28b2f38a5
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2022-05-26 13:06:03 +0100

    VB-819 Upload Hewell's visit schedule, update after review

--- src/main/resources/db.scripts.mvp/R__VB-819_add_hewell_code_HEI_prison_data.sql
+++ src/main/resources/db.scripts.mvp/R__VB-819_add_hewell_code_HEI_prison_data.sql
@@ -18,23 +18,23 @@
 -- Start a transaction
 BEGIN;
 
-        SET SCHEMA 'public';
+SET SCHEMA 'public';
 
-        DELETE FROM session_template WHERE prison_id='HEI';
+DELETE FROM session_template WHERE prison_id='HEI';
 
-        INSERT INTO public.session_template (prison_id, restrictions, visit_room, visit_type, open_capacity, closed_capacity, start_time, end_time, start_date, expiry_date, frequency)
-        VALUES
-            -- start_date MONDAY and then repeat weekly on Monday!
-            ('HEI', null, null, 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-05-30', null, 'WEEKLY'),
-            -- start_date WEDNESDAY and then repeat weekly on WEDNESDAY!
-            ('HEI', null, null, 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-01', null, 'WEEKLY'),
-            -- start_date FRIDAY then and repeat weekly on FRIDAY!
-            ('HEI', null, null, 'SOCIAL', 30, 3, '09:00:00', '11:00:00', '2022-06-03', null, 'WEEKLY'),
-            ('HEI', null, null, 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-03', null, 'WEEKLY'),
-            -- start_date SATURDAY then and repeat weekly on SATURDAY!
-            ('HEI', null, null, 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-04', null, 'WEEKLY'),
-            -- start_date SUNDAY then and repeat weekly on SUNDAY!
-            ('HEI', null, null, 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-05', null, 'WEEKLY');
+INSERT INTO public.session_template (prison_id, visit_room, visit_type, open_capacity, closed_capacity, start_time, end_time, start_date, expiry_date, frequency)
+VALUES
+    -- start_date MONDAY and then repeat weekly on Monday!
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-05-30', null, 'WEEKLY'),
+    -- start_date WEDNESDAY and then repeat weekly on WEDNESDAY!
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-01', null, 'WEEKLY'),
+    -- start_date FRIDAY then and repeat weekly on FRIDAY!
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '09:00:00', '11:00:00', '2022-06-03', null, 'WEEKLY'),
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-03', null, 'WEEKLY'),
+    -- start_date SATURDAY then and repeat weekly on SATURDAY!
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-04', null, 'WEEKLY'),
+    -- start_date SUNDAY then and repeat weekly on SUNDAY!
+    ('HEI', "visits main room", 'SOCIAL', 30, 3, '14:00:00', '16:00:00', '2022-06-05', null, 'WEEKLY');
 
 -- Commit the change
 END;
