ea10291b61d42304e45ad61a220bc666ebe9a43f
AledWEvans <101728271+AledWEvans@users.noreply.github.com>
2022-07-29 14:19:37 +0100

    upgrade-dependancies-from-4.3.3 (#148)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.3"
-  kotlin("jvm") version "1.7.0"
-  kotlin("plugin.spring") version "1.7.0"
-  kotlin("plugin.jpa") version "1.7.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1"
+  kotlin("jvm") version "1.7.10"
+  kotlin("plugin.spring") version "1.7.10"
+  kotlin("plugin.jpa") version "1.7.10"
   idea
 }
 
