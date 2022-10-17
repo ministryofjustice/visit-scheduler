398ddfa2038ef033dd85efc1b873adf54a0a687d
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2022-10-17 09:47:37 +0100

    VB-1309 VSiP Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.5"
   kotlin("jvm") version "1.7.10"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
