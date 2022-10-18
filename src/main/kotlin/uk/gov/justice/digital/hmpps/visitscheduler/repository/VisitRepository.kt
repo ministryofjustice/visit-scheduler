95976a7732fffe9d97b36cb45793e7d73f326710
AledWEvans <101728271+AledWEvans@users.noreply.github.com>
2022-10-18 10:52:30 +0100

    VB-1296 Technical Debt - Improve performance and reduce complexity of… (#181)
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries
    
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries, update SQL this should have had an error must an test for this
    
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries, added new integration test
    
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries, removed code that was not required
    
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries, Missing newline before ")"
    
    * VB-1296 Technical Debt - Improve performance and reduce complexity of DB queries, few tweaks after review

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/repository/VisitRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/repository/VisitRepository.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.visitscheduler.model.VisitStatus
@@ -98,11 +101,6 @@ interface VisitRepository : JpaRepository<Visit, Long>, JpaSpecificationExecutor
   )
   fun findReservedVisit(reference: String): Visit?
 
-  @Query(
-    "SELECT CASE WHEN (COUNT(v) = 1) THEN TRUE ELSE FALSE END  FROM Visit v WHERE v.reference = :bookingReference AND v.visitStatus = 'BOOKED' "
-  )
-  fun isValidBookingReference(bookingReference: String): Boolean
-
   @Transactional
   @Modifying
   @Query(
@@ -118,4 +116,38 @@ interface VisitRepository : JpaRepository<Visit, Long>, JpaSpecificationExecutor
     nativeQuery = true
   )
   fun updateCreateTimestamp(createTimestamp: LocalDateTime, id: Long): Int
+
+  @Query(
+    "SELECT v FROM Visit v LEFT JOIN v.visitors as vis " +
+      "WHERE (:visitStatus is null OR v.visitStatus = :visitStatus)  AND " +
+      "(:nomisPersonId is null OR vis.nomisPersonId = :nomisPersonId) AND " +
+      "(:prisonerId is null OR v.prisonerId = :prisonerId) AND " +
+      "(:prisonId is null OR v.prisonId = :prisonId) AND " +
+      "(cast(:startDateTime as date) is null OR v.visitStart >= :startDateTime) AND " +
+      "(cast(:endDateTime as date) is null OR v.visitStart < :endDateTime) "
+  )
+  fun findPageBy(
+    @Param("prisonerId") prisonerId: String? = null,
+    @Param("prisonId") prisonId: String,
+    @Param("startDateTime") startDateTime: LocalDateTime? = null,
+    @Param("endDateTime") endDateTime: LocalDateTime? = null,
+    @Param("visitStatus") visitStatus: VisitStatus? = null,
+    @Param("nomisPersonId") nomisPersonId: Long? = null,
+    page: Pageable
+  ): Page<Visit>
+
+  @Query(
+    "SELECT CASE WHEN (COUNT(v) > 0) THEN TRUE ELSE FALSE END FROM Visit v LEFT JOIN v.visitors as vis " +
+      "WHERE (v.visitStatus = 'BOOKED' OR v.visitStatus = 'RESERVED')  AND " +
+      "(v.prisonerId = :prisonerId) AND " +
+      "(v.prisonId = :prisonId) AND " +
+      "(v.visitStart >= :startDateTime) AND " +
+      "(cast(:endDateTime as date) is null OR v.visitStart < :endDateTime) "
+  )
+  fun hasVisits(
+    @Param("prisonerId") prisonerId: String,
+    @Param("prisonId") prisonId: String,
+    @Param("startDateTime") startDateTime: LocalDateTime,
+    @Param("endDateTime") endDateTime: LocalDateTime? = null
+  ): Boolean
 }
