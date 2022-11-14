7918a803c863f343ff5707f002ae9db34b428579
AledWEvans <101728271+AledWEvans@users.noreply.github.com>
2022-11-14 16:10:38 +0000

    Revert "VB-1431 Add new prison codes to DB (#211)"
    This reverts commit 4269984e39da743d5b1f05ed37e04a5db71dbe57.

--- src/main/resources/db/migration/data/R__add_prison_codes.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO prison(code, active) SELECT 'HEI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'HEI');
-INSERT INTO prison(code, active) SELECT 'BRS', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'BRS');
