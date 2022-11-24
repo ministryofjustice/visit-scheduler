2339183cea61a2e410aa98856566a25811ba7509
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2022-11-24 08:21:10 +0000

    VB-1434 Add code to server side to allow for bi-weekly session templates, removed not null as this is a speical table

--- src/main/resources/db/migration/V2_12__add_group_to_link_table.sql
+++ src/main/resources/db/migration/V2_12__add_group_to_link_table.sql
@@ -1,2 +1,2 @@
-ALTER TABLE session_to_permitted_location ADD location_group VARCHAR(80) NOT NULL;
+ALTER TABLE session_to_permitted_location ADD location_group VARCHAR(80);
 CREATE INDEX idx_permitted_location_group_id ON session_to_permitted_location(location_group);
