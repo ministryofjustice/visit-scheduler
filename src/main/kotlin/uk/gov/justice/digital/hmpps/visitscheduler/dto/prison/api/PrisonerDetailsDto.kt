2f60b7a617bfe1c34ca59dcebfb12918c0466c9a
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2022-12-01 16:51:58 +0000

    VB-1480 changes to make sure visit sessions are not being returned when the prisonId passed doesn't match the prisoners establishmentCode on Prison API.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/prison/api/PrisonerDetailsDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/prison/api/PrisonerDetailsDto.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.dto.prison.api
 
-
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Prisoner details - only offenderNo and location needed")
