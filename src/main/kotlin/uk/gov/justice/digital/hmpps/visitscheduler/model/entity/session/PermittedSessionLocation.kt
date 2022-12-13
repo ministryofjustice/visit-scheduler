45e92772923fdfb7f7c278e86301c799561fea88
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2022-12-13 12:44:36 +0000

    VB-1486 - organise imports.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/model/entity/session/PermittedSessionLocation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/model/entity/session/PermittedSessionLocation.kt
@@ -5,7 +5,17 @@ import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.jpa.repository.Temporal
 import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.Prison
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToMany
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+import javax.persistence.TemporalType
 
 @Entity
 @Table(name = "PERMITTED_SESSION_LOCATION")
