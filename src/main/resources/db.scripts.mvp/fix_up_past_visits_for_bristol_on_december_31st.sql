5d0f400243d4a79d59c07fd36c6a981d6d331c78
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-01-09 11:55:52 +0000

    VB-1563 Update HMP Bristol visits for 31/12/22

--- src/main/resources/db.scripts.mvp/fix_up_past_visits_for_bristol_on_december_31st.sql
+++ src/main/resources/db.scripts.mvp/fix_up_past_visits_for_bristol_on_december_31st.sql
@@ -2,11 +2,10 @@
 BEGIN;
 
     UPDATE visit
-        SET visit_start = date_trunc('day', s.visit_start) + '15:30:00',
-            visit_end = date_trunc('day', s.visit_end) + '16:30:00'
-            FROM (SELECT id,visit_start,visit_end FROM visit WHERE
-                    WHERE reference in ('jk-vs-lh-xa','nk-ws-lh-ow','oa-ks-li-dg','rv-ws-li-nv','qk-ws-lh-om','vk-ws-lu-xv','ok-ws-lf-ez','qy-ws-lc-sq','bv-ws-lh-dp','wv-ws-lu-ld','za-ks-lc-mx','mk-ws-li-zp','yk-ws-lu-ra')
-             ) AS s
-        WHERE visit.id = s.id AND prison_id = 2;
+        SET visit_start = date_trunc('day', visit_start) + '15:30:00',
+            visit_end = date_trunc('day', visit_end) + '16:30:00'
+        WHERE
+            reference in ('jk-vs-lh-xa','nk-ws-lh-ow','oa-ks-li-dg','rv-ws-li-nv','qk-ws-lh-om','vk-ws-lu-xv','ok-ws-lf-ez','qy-ws-lc-sq','bv-ws-lh-dp','wv-ws-lu-ld','za-ks-lc-mx','mk-ws-li-zp','yk-ws-lu-ra')
+            AND prison_id = 2;
 
 END;
