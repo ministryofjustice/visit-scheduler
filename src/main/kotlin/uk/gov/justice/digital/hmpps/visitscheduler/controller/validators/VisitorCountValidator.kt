b4d041ef8f8b4af57bb055c11ce2043d485527f6
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-01-17 11:08:51 +0000

    VB-1494 - updates based on review comments.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/controller/validators/VisitorCountValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/controller/validators/VisitorCountValidator.kt
@@ -14,24 +14,20 @@ annotation class VisitorCountValidation(
   val message: String = "{javax.validation.constraints.visitor.count.message}",
   val groups: Array<KClass<*>> = [],
   val payload: Array<KClass<out Payload>> = [],
-  val maximumVisitorsAllowed: UShort = VisitorCountValidator.MAXIMUM_VISITORS_ALLOWED,
+  val maximumVisitorsAllowed: Short = 10
 )
 
 class VisitorCountValidator : ConstraintValidator<VisitorCountValidation, Set<VisitorDto>> {
-  companion object {
-    const val MAXIMUM_VISITORS_ALLOWED: UShort = 10u
-  }
-  private var maximumVisitorsAllowed: UShort = MAXIMUM_VISITORS_ALLOWED
 
-  override fun initialize(constraintAnnotation: VisitorCountValidation?) {
-    constraintAnnotation?.let {
-      this.maximumVisitorsAllowed = constraintAnnotation.maximumVisitorsAllowed
-    }
+  private var maximumVisitorsAllowed: Short = 0
+
+  override fun initialize(constraintAnnotation: VisitorCountValidation) {
+    this.maximumVisitorsAllowed = constraintAnnotation.maximumVisitorsAllowed
   }
 
-  override fun isValid(childList: Set<VisitorDto>?, context: ConstraintValidatorContext?): Boolean {
+  override fun isValid(visitors: Set<VisitorDto>?, context: ConstraintValidatorContext?): Boolean {
     // Cannot have more than max visitors allowed
-    val total = (childList?.size ?: 0).toUShort()
-    return (total <= maximumVisitorsAllowed)
+    val totalVisitors = visitors?.size ?: 0
+    return (totalVisitors <= maximumVisitorsAllowed)
   }
 }
