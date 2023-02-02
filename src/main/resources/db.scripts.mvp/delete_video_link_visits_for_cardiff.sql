5d7dd0b897673e3594a0f9ac713dbe27855c28fb
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-02-02 11:41:07 +0000

    VB-1766 - delete CFI video calls from VSIP visits table and child tables.

---
+++ src/main/resources/db.scripts.mvp/delete_video_link_visits_for_cardiff.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+SET SCHEMA 'public';
+
+CREATE TEMP TABLE tmp_cfi_visit_ids_to_be_deleted(
+    visit_id         int
+);
+
+--selection criteria is CFI visits with a 30 minute window and room name as 'Video Link'
+--these visits should not be on VSIP as these are video calls and not VSIP visits
+INSERT INTO tmp_cfi_visit_ids_to_be_deleted (visit_id) (
+    select v.id from visit v
+    where prison_id in (
+        select prison_id from prison p where p.code = 'CFI'
+    )
+    and v.visit_start + interval '30 minutes' = v.visit_end
+    and visit_room = 'Video Link'
+    and v.id in (select ld.visit_id from legacy_data ld)
+);
+
+DELETE FROM visit_contact WHERE visit_id IN (SELECT visit_id FROM legacy_data)  and visit_id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+DELETE FROM visit_notes WHERE visit_id IN (SELECT visit_id FROM legacy_data)  and visit_id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+DELETE FROM visit_support WHERE visit_id IN (SELECT visit_id FROM legacy_data) and visit_id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+DELETE FROM visit_visitor WHERE visit_id IN (SELECT visit_id FROM legacy_data) and visit_id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+DELETE FROM visit WHERE id IN (SELECT visit_id FROM legacy_data) and id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+DELETE FROM legacy_data d where visit_id in (select visit_id FROM tmp_cfi_visit_ids_to_be_deleted);
+
+-- Drop temporary tables
+DROP TABLE tmp_cfi_visit_ids_to_be_deleted;
+
+END;
