c09f7346449659fff5bb1dd874112248b344c055
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-02-08 08:34:57 +0000

    Vb 1814 delete duplicate booking in production database (#275)
    * VB-1814 Delete duplicate booking in production database
    
    * VB-1814 Delete duplicate booking in production database

---
+++ src/main/resources/db.scripts.mvp/delete_duplicate_booking.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+    SET SCHEMA 'public';
+
+    CREATE TEMP TABLE tmp_visit_ids_to_be_deleted(
+        visit_id         int
+    );
+
+    --selection criteria for visits to be deleted
+    INSERT INTO tmp_visit_ids_to_be_deleted (visit_id) (
+        select v.id from visit v
+                join legacy_data ld on ld.visit_id = v.id
+                where v.reference = 'jr-wd-ri-zl'
+    );
+
+    DELETE FROM visit_contact WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_notes WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_support WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_visitor WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit WHERE id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM legacy_data d where visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+
+    -- Drop temporary tables
+    DROP TABLE tmp_visit_ids_to_be_deleted;
+
+END;
