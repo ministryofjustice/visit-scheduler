080b698c35b713e4a7f79852d3fa7caf20cdc743
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-03-20 10:06:24 +0000

    VB-1921 - updated code to send CancelVisitDto as part of cancel requests.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/CancelVisitDto.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+
+data class CancelVisitDto (
+  @Schema(description = "Outcome - status and text", required = true)
+  @field:Valid
+  val cancelOutcome: OutcomeDto,
+
+  @Schema(description = "Username for user who actioned this request", required = true)
+  @field:NotBlank
+  val actionedBy: String,
+)
