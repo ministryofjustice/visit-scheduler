2fa009c5eed06935c1f0eb6837494115ce1a89c4
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-04-18 11:23:46 +0100

    Sql scripts to add prisons and turn them on when neeeded (#335)
    * Sql scripts to add prisons and turn them on when neeeded
    
    * Sql scripts to add prisons and turn them on when neeeded

--- src/main/resources/db/migration/data/R__add_prison_codes.sql
+++ src/main/resources/db/migration/data/R__add_prison_codes.sql
@@ -3,8 +3,15 @@ INSERT INTO prison(code, active) SELECT 'BLI', true WHERE NOT EXISTS ( SELECT id
 INSERT INTO prison(code, active) SELECT 'CFI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'CFI');
 INSERT INTO prison(code, active) SELECT 'WWI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'WWI');
 INSERT INTO prison(code, active) SELECT 'PNI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'PNI');
+INSERT INTO prison(code, active) SELECT 'EWI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'EWI');
+INSERT INTO prison(code, active) SELECT 'DHI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'DHI');
+INSERT INTO prison(code, active) SELECT 'MHI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'MHI');
+INSERT INTO prison(code, active) SELECT 'BNI', true WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'BNI');
 -- Disabled for now below this line
-INSERT INTO prison(code, active) SELECT 'EWI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'EWI');
-INSERT INTO prison(code, active) SELECT 'DHI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'DHI');
-INSERT INTO prison(code, active) SELECT 'MHI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'MHI');
-INSERT INTO prison(code, active) SELECT 'BNI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'BNI');
+INSERT INTO prison(code, active) SELECT 'LNI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'LNI');
+INSERT INTO prison(code, active) SELECT 'FHI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'FHI');
+INSERT INTO prison(code, active) SELECT 'ESI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'ESI');
+INSERT INTO prison(code, active) SELECT 'DMI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'DMI');
+INSERT INTO prison(code, active) SELECT 'BSI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'BSI');
+INSERT INTO prison(code, active) SELECT 'AGI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'AGI');
+INSERT INTO prison(code, active) SELECT 'FNI', false WHERE NOT EXISTS ( SELECT id FROM prison WHERE code = 'FNI');
