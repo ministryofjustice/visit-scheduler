64545df8bbf27b37d5d08685617d3ee5e9c89a1f
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-04-18 11:13:55 +0100

    Sql scripts to add prisons and turn them on when neeeded

--- src/main/resources/db.scripts.mvp/switch_on_on_board_prisons.sql
+++ src/main/resources/db.scripts.mvp/switch_on_on_board_prisons.sql
@@ -3,6 +3,6 @@ BEGIN;
     SET SCHEMA 'public';
 
     UPDATE prison SET active = true
-        WHERE code in ('LNI','FHI','ESI','DMI','BSI','AGI','FNI')
+        WHERE code in ('LNI','FHI','ESI','DMI','BSI','AGI','FNI');
 
 END;
