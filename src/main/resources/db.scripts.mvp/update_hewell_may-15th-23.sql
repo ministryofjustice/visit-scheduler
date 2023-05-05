1ac94b21aac20672db14135c4433719fabd15ae7
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-05-05 15:42:12 +0100

    Update HMP Hewell capacity for Monday 15/5/23

--- src/main/resources/db.scripts.mvp/update_hewell_may-15th-23.sql
+++ src/main/resources/db.scripts.mvp/update_hewell_may-15th-23.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
     SET SCHEMA 'public';
-    UPDATE session_template SET valid_to_date = '2023-03-24' WHERE id=20 AND prison_id = 1;
+    UPDATE session_template SET valid_to_date = '2023-05-14' WHERE id=20 AND prison_id = (select id from prison where code = 'HEI') and day_of_week = 'MONDAY';
 
 
     INSERT INTO session_template (prison_id,visit_room, visit_type, open_capacity, closed_capacity,enhanced, start_time, end_time, valid_from_date, valid_to_date, day_of_week,bi_weekly,name,reference)
-    values (1,'Visits Main Room','SOCIAL',28,4,false,'13:45','14:45','2023-05-15','2023-05-15','MONDAY',false,'MONDAY 15th Special'      ,'-afe-insert.09'),
-           (1,'Visits Main Room','SOCIAL',35,2,false,'13:45','14:45','2023-05-16',null        ,'MONDAY',false,'MONDAY, 2023-05-16, 13:45','-afe-insert.10');
+    values ((select id from prison where code = 'HEI'),'Visits Main Room','SOCIAL',28,4,false,'13:45','14:45','2023-05-15','2023-05-15','MONDAY',false,'MONDAY 15th Special'      ,'-afe-insert.09'),
+           ((select id from prison where code = 'HEI'),'Visits Main Room','SOCIAL',35,2,false,'13:45','14:45','2023-05-16',null        ,'MONDAY',false,'MONDAY, 2023-05-16, 13:45','-afe-insert.10');
 
 END;
