ed278cc985fc35aeccb62a8ef4c2512e7150fa63
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-05-17 07:33:50 +0100

    Added new logs to help test when a prisoner is moved, error in logs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/utils/MigrationSessiontTemplateMatcher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/utils/MigrationSessiontTemplateMatcher.kt
@@ -141,7 +141,7 @@ class MigrationSessionTemplateMatcher(
     if (prisonerDto == null) {
       throw MatchSessionTemplateToMigratedVisitException("getNearestSessionTemplate : Prisoner cannot be found : $message!")
     } else if (prisonCode != prisonerDto.prisonCode) {
-      LOG.debug("getNearestSessionTemplate : migrated prison ($prisonCode) is different from prisoners location prison ($prisonerDto.prisonCode)!")
+      LOG.debug("getNearestSessionTemplate : migrated $prisonerId prison ($prisonCode) has a different location prison ($prisonerDto)!")
     }
 
     val prisonerDetailDto = prisonerService.getPrisonerHousingLocation(prisonerId, prisonCode)!!
