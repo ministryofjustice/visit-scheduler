7876d46d724c1fa87f09fbd2cc65ed77267a0b5e
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-07-05 12:15:08 +0100

    VB-2054 Store contact method information in the service, change default value in scrip to NOT_KNOWN

--- src/main/resources/db/migration/V2_29__add_application_methodactive_to_visit.sql
+++ src/main/resources/db/migration/V2_29__add_application_methodactive_to_visit.sql
@@ -1,3 +1,3 @@
 ALTER TABLE visit ADD last_application_method varchar(90);
-UPDATE visit SET last_application_method = 'UN_KNOWN';
+UPDATE visit SET last_application_method = 'NOT_KNOWN';
 ALTER TABLE visit ALTER COLUMN last_application_method SET NOT NULL
