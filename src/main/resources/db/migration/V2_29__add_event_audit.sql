2154c2bda27f75fe1c9fe87d92f10346b1b54bd4
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-07-11 17:02:10 +0100

    VB-842 Calculate the cost of our service, updates to sql script after review

--- src/main/resources/db/migration/V2_29__add_event_audit.sql
+++ src/main/resources/db/migration/V2_29__add_event_audit.sql
@@ -14,31 +14,43 @@ CREATE INDEX idx_booking_reference ON event_audit(booking_reference);
 CREATE INDEX idx_visit_application_reference ON event_audit(application_reference);
 CREATE INDEX idx_visit_session_template_reference ON event_audit(session_template_reference);
 
+CREATE TEMP TABLE tmp_updated_visits(visit_id int not null);
+
+INSERT INTO tmp_updated_visits(visit_id)
+    select v.id from (
+                         select reference  from visit where (visit_status = 'BOOKED' or outcome_status='SUPERSEDED_CANCELLATION')
+                         group by reference  having count(reference) > 1) as tmp
+                         join visit v on v.reference = tmp.reference
+    where visit_status = 'BOOKED'
+    order by v.reference,v.id
+
 -- Copy data over from session_template table
 INSERT INTO event_audit (booking_reference, application_reference, session_template_reference, type, application_method_type, actioned_by, create_timestamp)
-    (SELECT reference,
-            application_reference,
-            session_template_reference,
-            CASE
-                WHEN visit_status   = 'RESERVED' THEN 'RESERVED_VISIT'
-                WHEN visit_status   = 'CHANGING' THEN 'CHANGING_VISIT'
-                WHEN visit_status   = 'BOOKED' THEN 'BOOKED_VISIT'
-                WHEN outcome_status = 'SUPERSEDED_CANCELLATION' THEN 'UPDATED_VISIT'
-                WHEN visit_status   = 'CANCELLED' THEN 'CANCELED_VISIT'
-                END as type,
-            'NOT_KNOWN' AS application_method_type,
-            CASE
-                WHEN visit_status   = 'CANCELLED' THEN cancelled_by
-                WHEN outcome_status = 'SUPERSEDED_CANCELLATION' THEN updated_by
-                WHEN updated_by is not null THEN updated_by
-                ELSE created_by
-                END AS actioned_by,
-            CASE
-                WHEN modify_timestamp is null THEN create_timestamp
-                ELSE modify_timestamp
-                END AS create_timestamp
-     FROM visit
-      ORDER BY create_timestamp DESC);
+    (SELECT 	    application_reference,
+     session_template_reference,
+     CASE
+         WHEN visit_status   = 'RESERVED' THEN 'RESERVED_VISIT'
+         WHEN visit_status   = 'CHANGING' THEN 'CHANGING_VISIT'
+         WHEN outcome_status = 'SUPERSEDED_CANCELLATION'  THEN 'BOOKED_VISIT'
+         WHEN visit_status  = 'CANCELLED' THEN 'CANCELLED_VISIT'
+         WHEN id in (select * from tmp_updated_visits) THEN 'UPDATED_VISIT'
+        else  'BOOKED_VISIT'
+    END as type,
+    'NOT_KNOWN' AS application_method_type,
+    case
+        WHEN visit_status   = 'RESERVED' THEN created_by
+        WHEN visit_status   = 'CHANGING' THEN created_by
+        WHEN outcome_status = 'SUPERSEDED_CANCELLATION' THEN created_by
+        WHEN visit_status  = 'CANCELLED' THEN cancelled_by
+        WHEN id in (select * from tmp_updated_visits) THEN updated_by
+        else  created_by
+    END AS actioned_by,
+    CASE
+        WHEN modify_timestamp is null THEN create_timestamp
+        ELSE modify_timestamp
+    END AS create_timestamp
+    FROM visit v  	where created_by is not null
+    ORDER BY reference desc);
 
 UPDATE event_audit	SET type = 'MIGRATED_VISIT'
     FROM (select reference  from visit v join legacy_data ld on ld.visit_id  = v.id WHERE v.visit_status  = 'BOOKED') AS v
@@ -47,3 +59,5 @@ WHERE event_audit.booking_reference = v.reference;
 ALTER TABLE visit DROP created_by;
 ALTER TABLE visit DROP updated_by;
 ALTER TABLE visit DROP cancelled_by;
+
+
