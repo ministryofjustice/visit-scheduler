9c4e35bb7bf414dc519448b9c6617febfbaf0544
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-07-13 11:08:16 +0100

    VB-2450 Update visit schedule for HMP Morton Hall (MHI) in VSIP

--- src/main/resources/db.scripts.mvp/update_visit_times_mhi.sql
+++ src/main/resources/db.scripts.mvp/update_visit_times_mhi.sql
@@ -1,8 +1,8 @@
 -- This sql makes sure visits in future aline up to the latest session times and session templates.
 
 UPDATE visit
-    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00'
-    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00'
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
     SET session_template_reference = <PLEASE NETER ST REF FOR TUESDAY>
 FROM (SELECT v.* FROM visit v
     LEFT JOIN legacy_data ld ON ld.visit_id = v.id
@@ -11,9 +11,9 @@ FROM (SELECT v.* FROM visit v
 WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'TUESDAY';
 
 UPDATE visit
-    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00'
-    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00'
-SET session_template_reference = <PLEASE NETER ST REF FOR THURSDAY>
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    SET session_template_reference = <PLEASE NETER ST REF FOR THURSDAY>
 FROM (SELECT v.* FROM visit v
     LEFT JOIN legacy_data ld ON ld.visit_id = v.id
     JOIN prison p ON p.id = v.prison_id
@@ -21,9 +21,9 @@ FROM (SELECT v.* FROM visit v
 WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'THURSDAY';
 
 UPDATE visit
-    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00'
-    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00'
-SET session_template_reference = <PLEASE NETER ST REF FOR SATURDAY>
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    SET session_template_reference = <PLEASE NETER ST REF FOR SATURDAY>
 FROM (SELECT v.* FROM visit v
     LEFT JOIN legacy_data ld ON ld.visit_id = v.id
     JOIN prison p ON p.id = v.prison_id
@@ -31,9 +31,9 @@ FROM (SELECT v.* FROM visit v
 WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'SATURDAY';
 
 UPDATE visit
-    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00'
-    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00'
-SET session_template_reference = <PLEASE NETER ST REF FOR SUNDAY>
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    SET visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    SET session_template_reference = <PLEASE NETER ST REF FOR SUNDAY>
 FROM (SELECT v.* FROM visit v
     LEFT JOIN legacy_data ld ON ld.visit_id = v.id
     JOIN prison p ON p.id = v.prison_id
