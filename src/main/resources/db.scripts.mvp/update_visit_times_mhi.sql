7f6fea27fb4295580ba1005be4b7e73aacf07b46
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-07-28 13:18:22 +0100

    Merge main into develop (#499)

---
+++ src/main/resources/db.scripts.mvp/update_visit_times_mhi.sql
@@ -0,0 +1,43 @@
+-- This sql makes sure visits in future aline up to the latest session times and session templates.
+
+UPDATE visit
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    session_template_reference = <PLEASE NETER ST REF FOR TUESDAY>
+FROM (SELECT v.* FROM visit v
+    LEFT JOIN legacy_data ld ON ld.visit_id = v.id
+    JOIN prison p ON p.id = v.prison_id
+    WHERE p.code = 'MHI' AND v.visit_start > CURRENT_DATE AND ld.id IS NULL) AS tmp
+WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'TUESDAY';
+
+UPDATE visit
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    session_template_reference = <PLEASE NETER ST REF FOR THURSDAY>
+FROM (SELECT v.* FROM visit v
+    LEFT JOIN legacy_data ld ON ld.visit_id = v.id
+    JOIN prison p ON p.id = v.prison_id
+    WHERE p.code = 'MHI' AND v.visit_start > CURRENT_DATE AND ld.id IS NULL) AS tmp
+WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'THURSDAY';
+
+UPDATE visit
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    session_template_reference = <PLEASE NETER ST REF FOR SATURDAY>
+FROM (SELECT v.* FROM visit v
+    LEFT JOIN legacy_data ld ON ld.visit_id = v.id
+    JOIN prison p ON p.id = v.prison_id
+    WHERE p.code = 'MHI' AND v.visit_start > CURRENT_DATE AND ld.id IS NULL) AS tmp
+WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'SATURDAY';
+
+UPDATE visit
+    SET visit_start = date_trunc('day', tmp.visit_start) + '14:00:00',
+    visit_end = date_trunc('day', tmp.visit_end) + '16:00:00',
+    session_template_reference = <PLEASE NETER ST REF FOR SUNDAY>
+FROM (SELECT v.* FROM visit v
+    LEFT JOIN legacy_data ld ON ld.visit_id = v.id
+    JOIN prison p ON p.id = v.prison_id
+    WHERE p.code = 'MHI' AND v.visit_start > CURRENT_DATE AND ld.id IS NULL) AS tmp
+WHERE tmp.id = visit.id and UPPER(TRIM(To_Char(tmp.visit_start , 'DAY'))) = 'SUNDAY';
+
+
