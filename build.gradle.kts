480c5b29576879d4f373de04a018ec50076de1b1
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-09-14 12:57:22 +0100

    Update dependencies based on dps-gradle-spring-boot updates as a fix for security issues.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0-beta"
-  kotlin("plugin.spring") version "1.9.0"
-  kotlin("plugin.jpa") version "1.9.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.1"
+  kotlin("plugin.spring") version "1.9.10"
+  kotlin("plugin.jpa") version "1.9.10"
   idea
 }
 
@@ -28,17 +28,17 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.28.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.30.0")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-  implementation("org.springframework.data:spring-data-commons:3.1.2")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.1.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
+  implementation("org.springframework.data:spring-data-commons:3.1.3")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("net.javacrumbs.shedlock:shedlock-spring:5.6.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.6.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.7.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.7.0")
   implementation("org.apache.commons:commons-csv:1.10.0")
   implementation("org.freemarker:freemarker:2.3.32")
 
@@ -48,7 +48,7 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.16")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.testcontainers:localstack:1.18.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
@@ -56,20 +56,20 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.18.3")
-  testImplementation("org.testcontainers:localstack:1.18.3")
-  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.513")
+  testImplementation("org.testcontainers:postgresql:1.19.0")
+  testImplementation("org.testcontainers:localstack:1.19.0")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.550")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(20))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "19"
+      jvmTarget = "20"
     }
   }
 }
