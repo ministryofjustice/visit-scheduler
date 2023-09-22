26612e053d225f2719622e71ec1069bca5fccc65
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-09-22 15:20:35 +0100

    Vb 1536 proactively manage bookings, added endpoints for new notifications (#534)
    * VB-1536 Proactively manage bookings, added enpoints for new notifications
    
    * VB-1536 Proactively manage bookings, added enpoints for new notifications

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/visitnotification/PrisonerReceivedNotificationDto.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.dto.visitnotification
+
+import jakarta.validation.constraints.NotBlank
+
+data class PrisonerReceivedNotificationDto(
+  @NotBlank
+  val prisonerNumber: String,
+)
