26e866df6e6bbbca9971f0b1dc442ce600df7616
Aled <101728271+AledWEvans@users.noreply.github.com>
2023-10-26 17:11:57 +0100

    VB-2762 We were not including the start and end date (#566)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitService.kt
@@ -495,7 +495,7 @@ class VisitService(
     return visitCancellationDateAllowed
   }
 
-  fun getFutureVisitsBy(prisonerNumber: String, prisonCode: String?): List<VisitDto> {
-    return this.visitRepository.getVisits(prisonerNumber, prisonCode, LocalDateTime.now()).map { VisitDto(it) }
+  fun getFutureVisitsBy(prisonerNumber: String, prisonCode: String?, startDateTime: LocalDateTime = LocalDateTime.now(), endDateTime: LocalDateTime ? = null): List<VisitDto> {
+    return this.visitRepository.getVisits(prisonerNumber, prisonCode, startDateTime, endDateTime).map { VisitDto(it) }
   }
 }
