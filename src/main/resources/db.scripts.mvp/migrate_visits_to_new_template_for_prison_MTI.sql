9d5139c933acc540ace693e8d850503b7af57b75
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-11-03 15:14:26 +0000

    VB-3183 Migrate 2 visits from one session template to another

---
+++ src/main/resources/db.scripts.mvp/migrate_visits_to_new_template_for_prison_MTI.sql
@@ -0,0 +1,8 @@
+-- https://dsdmoj.atlassian.net/browse/VB-3183 Migrate 2 visits from one session template to another
+BEGIN;
+
+    UPDATE visit
+        SET session_template_reference = 'gsa.hlr.pmj'
+        WHERE reference in ('dy-lp-xa-cl','mb-lp-xa-un') and reference = 'jqa.hmm.mmp';
+
+END;
