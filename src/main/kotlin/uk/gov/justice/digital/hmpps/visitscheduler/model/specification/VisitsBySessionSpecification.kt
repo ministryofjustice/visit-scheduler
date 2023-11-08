220081ecb7db2a34d76a8d6ce2dbbe2e1424561f
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-11-08 12:57:02 +0000

    VB-2763 add a new endpoint that will allow visits to be returned for a session template for a date / date range.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/model/specification/VisitsBySessionSpecification.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.model.specification
+
+import jakarta.persistence.criteria.CriteriaBuilder
+import jakarta.persistence.criteria.CriteriaQuery
+import jakarta.persistence.criteria.Predicate
+import jakarta.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specification
+import uk.gov.justice.digital.hmpps.visitscheduler.model.VisitsBySessionFilter
+import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.Visit
+import java.time.LocalTime
+
+class VisitsBySessionSpecification(private val filter: VisitsBySessionFilter) : Specification<Visit> {
+  override fun toPredicate(
+    root: Root<Visit>,
+    query: CriteriaQuery<*>,
+    criteriaBuilder: CriteriaBuilder,
+  ): Predicate? {
+    val predicates = mutableListOf<Predicate>()
+
+    with(filter) {
+      sessionTemplateReference.run {
+        predicates.add(criteriaBuilder.equal(root.get<String>(Visit::sessionTemplateReference.name), sessionTemplateReference))
+      }
+
+      fromDate?.run {
+        predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(Visit::visitStart.name), fromDate.atStartOfDay()))
+      }
+
+      toDate?.run {
+        predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(Visit::visitStart.name), toDate.atTime(LocalTime.MAX)))
+      }
+
+      if (visitStatusList.isNotEmpty()) {
+        val visitStatusPath = root.get<String>(Visit::visitStatus.name)
+        predicates.add(visitStatusPath.`in`(visitStatusList))
+      }
+    }
+
+    return criteriaBuilder.and(*predicates.toTypedArray())
+  }
+}
