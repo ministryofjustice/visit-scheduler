a2c8d040aa52827ed4f17d8db437df4f677dd4fa
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-01-15 09:29:36 +0000

    VB-3366,VB-3365,VB-3368 flyway script, migration of data code and DB…

--- src/main/resources/db/migration/V3_02__insert_data_into_tmp_visit_table.sql
+++ src/main/resources/db/migration/V3_02__insert_data_into_tmp_visit_table.sql
@@ -22,6 +22,7 @@ FROM   (SELECT V.*,ss.id AS new_session_slot_id FROM visit v
                         JOIN session_slot ss on  (
                              ss.session_template_reference IS NULL AND
                              ss.prison_id 		= v.prison_id AND
+                             ss.slot_date 		= visit_start::DATE AND
                              ss.slot_time 		= visit_start::TIME AND
                              ss.slot_end_time  	= visit_end::TIME
     ) WHERE  v.session_template_reference IS null) tmp;
@@ -51,7 +52,8 @@ FROM   (
                        ss.session_template_reference = v.session_template_reference AND
                        ss.prison_id 		= v.prison_id AND
                        ss.slot_date 		= visit_start::DATE
-               )) tmp;
+               ) WHERE  v.session_template_reference IS NOT null
+        ) tmp;
 
 
 INSERT INTO tmp_visit SELECT * FROM tmp_visit_no_order order by id;
