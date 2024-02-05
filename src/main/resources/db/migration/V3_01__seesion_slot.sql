da79f04267894b3bf1e6680b4a50aec367761ffb
Aled <101728271+AledWEvans@users.noreply.github.com>
2024-02-05 10:47:54 +0000

    VB-3335 Defect found slot sql, duplicate session slot were being prod… (#624)
    VB-3335 Defect found slot sql, duplicate session slot were being produced

--- src/main/resources/db/migration/V3_01__seesion_slot.sql
+++ src/main/resources/db/migration/V3_01__seesion_slot.sql
@@ -8,26 +8,33 @@ CREATE TABLE session_slot_no_order
     slot_end                    timestamp       with time zone NOT NULL
 );
 
--- Insert session info data for applications that are migrated -- time taken 6+ min's
+-- Insert session info data for applications that are migrated
 INSERT INTO session_slot_no_order (prison_id,slot_date,slot_start,slot_end)
     SELECT v.prison_id,v.visit_start::date,v.visit_start,v.visit_end FROM visit v
         WHERE v.session_template_reference IS NULL
         GROUP BY v.visit_start::DATE,v.visit_start,v.visit_end,prison_id ORDER BY v.visit_start;
 
--- Insert session info data from tmp_application and session templates -- time taken 7+ min's
+-- Insert session info data from tmp_application and session templates
 INSERT INTO session_slot_no_order (session_template_reference,prison_id,slot_date,slot_start,slot_end)
-    SELECT st.reference AS session_template_reference,
-           v.prison_id,
-           v.visit_start::date,
-           v.visit_start::date + st.start_time,
-           v.visit_end::date + st.end_time
+        SELECT st.reference AS session_template_reference,
+               v.prison_id,
+               v.visit_start::date,
+                v.visit_start::date + st.start_time,
+                v.visit_end::date + st.end_time
         FROM visit v
                  JOIN session_template st ON st.reference = v.session_template_reference
-        GROUP BY st.reference,v.visit_start,v.visit_end ,st.start_time,st.end_time,v.prison_id
+        GROUP BY
+            st.reference,
+            v.prison_id,
+            v.visit_start::date,
+            v.visit_end::date,
+            st.start_time,
+            st.end_time
         ORDER BY v.visit_start::DATE;
 
 INSERT INTO session_slot (reference,session_template_reference,prison_id,slot_date,slot_start,slot_end)
-        SELECT REGEXP_REPLACE(to_hex(id+2951597050), '(.{3})(?!$)', '\1~','g'),session_template_reference,prison_id,slot_date,slot_start,slot_end FROM session_slot_no_order ORDER BY slot_date,slot_start;
+        SELECT REGEXP_REPLACE(to_hex(id+2951597050), '(.{3})(?!$)', '\1~','g'),session_template_reference,prison_id,slot_date,slot_start,slot_end
+                FROM session_slot_no_order ORDER BY slot_start,prison_id,session_template_reference;
 
 TRUNCATE TABLE session_slot_no_order;
 DROP TABLE session_slot_no_order;
