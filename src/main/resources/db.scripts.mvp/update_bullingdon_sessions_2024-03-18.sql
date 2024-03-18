d4d7537febe9884d129ec6d8e1fa254f2c751626
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-03-18 17:25:16 +0000

    Get prison ID from DB

--- src/main/resources/db.scripts.mvp/update_bullingdon_sessions_2024-03-18.sql
+++ src/main/resources/db.scripts.mvp/update_bullingdon_sessions_2024-03-18.sql
@@ -10,10 +10,10 @@
 BEGIN;
 SET SCHEMA 'public';
 
-UPDATE session_slot set session_template_reference = 'qxl.ubv.dse' where session_template_reference = 'wjc.yvp.rbr' and slot_date >= '2024-03-19' and prison_id = 9;
-UPDATE session_slot set session_template_reference = 'axl.feg.jej' where session_template_reference = 'qji.qdx.npj' and slot_date >= '2024-03-19' and prison_id = 9;
-UPDATE session_slot set session_template_reference = 'lxl.fwb.zmb' where session_template_reference = 'bjc.zsm.gqz' and slot_date >= '2024-03-19' and prison_id = 9;
-UPDATE session_slot set session_template_reference = 'yxl.ims.nox' where session_template_reference = 'ejc.qys.yxx' and slot_date >= '2024-03-19' and prison_id = 9;
-UPDATE session_slot set session_template_reference = 'mxl.hay.ozm' where session_template_reference = 'jjc.gqj.xqs' and slot_date >= '2024-03-19' and prison_id = 9;
+UPDATE session_slot set session_template_reference = 'qxl.ubv.dse' where session_template_reference = 'wjc.yvp.rbr' and slot_date >= '2024-03-19' and prison_id = (select id from prison where code = 'BNI');
+UPDATE session_slot set session_template_reference = 'axl.feg.jej' where session_template_reference = 'qji.qdx.npj' and slot_date >= '2024-03-19' and prison_id = (select id from prison where code = 'BNI');
+UPDATE session_slot set session_template_reference = 'lxl.fwb.zmb' where session_template_reference = 'bjc.zsm.gqz' and slot_date >= '2024-03-19' and prison_id = (select id from prison where code = 'BNI');
+UPDATE session_slot set session_template_reference = 'yxl.ims.nox' where session_template_reference = 'ejc.qys.yxx' and slot_date >= '2024-03-19' and prison_id = (select id from prison where code = 'BNI');
+UPDATE session_slot set session_template_reference = 'mxl.hay.ozm' where session_template_reference = 'jjc.gqj.xqs' and slot_date >= '2024-03-19' and prison_id = (select id from prison where code = 'BNI');
 
 END;
