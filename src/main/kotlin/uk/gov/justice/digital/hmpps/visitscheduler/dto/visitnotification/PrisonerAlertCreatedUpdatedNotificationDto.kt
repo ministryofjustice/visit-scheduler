404aef9920ed0b386d659ec5c44175f78c0fa0c1
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-05-30 15:30:19 +0100

    Feature: VB-3763 - Prisoner alert change events are processed
    Introduce an enum set for the alertsAdded which we will receive in the Dto.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/visitnotification/PrisonerAlertCreatedUpdatedNotificationDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/visitnotification/PrisonerAlertCreatedUpdatedNotificationDto.kt
@@ -3,11 +3,12 @@ package uk.gov.justice.digital.hmpps.visitscheduler.dto.visitnotification
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import jakarta.validation.constraints.NotBlank
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.PrisonerAlertCodeType
 
 data class PrisonerAlertCreatedUpdatedNotificationDto(
   @NotBlank
   val prisonerNumber: String,
 
   @JsonInclude(Include.NON_NULL)
-  val alertsAdded: List<String>? = emptyList(),
+  val alertsAdded: List<PrisonerAlertCodeType>? = emptyList(),
 )
