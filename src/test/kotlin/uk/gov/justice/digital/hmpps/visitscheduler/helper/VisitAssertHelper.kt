8b41af85aa1e163b6b93843cb1460a35c809a09a
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-06-06 14:42:48 +0100

    VB-3916 Get visits by booker reference

--- src/test/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/helper/VisitAssertHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/helper/VisitAssertHelper.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.helper
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitDto
 import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.Visit
@@ -9,43 +9,43 @@ import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.Visit
 class VisitAssertHelper {
 
   fun assertVisitDto(visitDto: VisitDto, visit: Visit) {
-    Assertions.assertThat(visitDto.reference).isEqualTo(visit.reference)
-    Assertions.assertThat(visitDto.applicationReference).isEqualTo(visit.getLastApplication()?.reference)
-    Assertions.assertThat(visitDto.prisonerId).isEqualTo(visit.prisonerId)
-    Assertions.assertThat(visitDto.prisonCode).isEqualTo(visit.prison.code)
-    Assertions.assertThat(visitDto.visitRoom).isEqualTo(visit.visitRoom)
-    Assertions.assertThat(visitDto.startTimestamp).isEqualTo(visit.sessionSlot.slotStart)
-    Assertions.assertThat(visitDto.endTimestamp).isEqualTo(visit.sessionSlot.slotEnd)
-    Assertions.assertThat(visitDto.visitType).isEqualTo(visit.visitType)
-    Assertions.assertThat(visitDto.visitStatus).isEqualTo(visit.visitStatus)
-    Assertions.assertThat(visitDto.visitRestriction).isEqualTo(visit.visitRestriction)
+    assertThat(visitDto.reference).isEqualTo(visit.reference)
+    assertThat(visitDto.applicationReference).isEqualTo(visit.getLastApplication()?.reference)
+    assertThat(visitDto.prisonerId).isEqualTo(visit.prisonerId)
+    assertThat(visitDto.prisonCode).isEqualTo(visit.prison.code)
+    assertThat(visitDto.visitRoom).isEqualTo(visit.visitRoom)
+    assertThat(visitDto.startTimestamp).isEqualTo(visit.sessionSlot.slotStart)
+    assertThat(visitDto.endTimestamp).isEqualTo(visit.sessionSlot.slotEnd)
+    assertThat(visitDto.visitType).isEqualTo(visit.visitType)
+    assertThat(visitDto.visitStatus).isEqualTo(visit.visitStatus)
+    assertThat(visitDto.visitRestriction).isEqualTo(visit.visitRestriction)
 
-    Assertions.assertThat(visitDto.visitContact.name).isEqualTo(visit.visitContact!!.name)
-    Assertions.assertThat(visitDto.visitContact.telephone).isEqualTo(visit.visitContact!!.telephone)
+    assertThat(visitDto.visitContact.name).isEqualTo(visit.visitContact!!.name)
+    assertThat(visitDto.visitContact.telephone).isEqualTo(visit.visitContact!!.telephone)
 
-    Assertions.assertThat(visitDto.visitors.size).isEqualTo(visit.visitors.size)
+    assertThat(visitDto.visitors.size).isEqualTo(visit.visitors.size)
     visit.visitors.forEachIndexed { index, visitVisitor ->
-      Assertions.assertThat(visitDto.visitors[index].nomisPersonId).isEqualTo(visitVisitor.nomisPersonId)
-      Assertions.assertThat(visitDto.visitors[index].visitContact).isEqualTo(visitVisitor.visitContact)
+      assertThat(visitDto.visitors[index].nomisPersonId).isEqualTo(visitVisitor.nomisPersonId)
+      assertThat(visitDto.visitors[index].visitContact).isEqualTo(visitVisitor.visitContact)
     }
 
     visit.support?.let {
-      Assertions.assertThat(visitDto.visitorSupport?.description).isEqualTo(it.description)
+      assertThat(visitDto.visitorSupport?.description).isEqualTo(it.description)
     }
 
-    Assertions.assertThat(visitDto.visitNotes.size).isEqualTo(visit.visitNotes.size)
+    assertThat(visitDto.visitNotes.size).isEqualTo(visit.visitNotes.size)
     visit.visitNotes.forEachIndexed { index, visitNote ->
-      Assertions.assertThat(visitDto.visitNotes[index].type).isEqualTo(visitNote.type)
-      Assertions.assertThat(visitDto.visitNotes[index].text).isEqualTo(visitNote.text)
+      assertThat(visitDto.visitNotes[index].type).isEqualTo(visitNote.type)
+      assertThat(visitDto.visitNotes[index].text).isEqualTo(visitNote.text)
     }
 
     visit.outcomeStatus?.let {
-      Assertions.assertThat(visitDto.outcomeStatus).isEqualTo(visit.outcomeStatus)
+      assertThat(visitDto.outcomeStatus).isEqualTo(visit.outcomeStatus)
     }
 
-    Assertions.assertThat(visitDto.createdTimestamp).isEqualTo(visit.createTimestamp)
-    Assertions.assertThat(visitDto.modifiedTimestamp).isNotNull()
+    assertThat(visitDto.createdTimestamp).isEqualTo(visit.createTimestamp)
+    assertThat(visitDto.modifiedTimestamp).isNotNull()
 
-    Assertions.assertThat(visitDto.userType).isEqualTo(visit.userType)
+    assertThat(visitDto.userType).isEqualTo(visit.userType)
   }
 }
