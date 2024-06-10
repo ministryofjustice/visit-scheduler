d36ae41b90ab7386afcdd72a7d34baf69c2a2164
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-06-10 09:02:28 +0100

    Feature: VB-2761 - Prisoner transferred events are processed
    Add a JsonProperty to the lastPrisonCode as the client sends it as lastPrisonId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/prisonersearch/PrisonerSearchResultDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/prisonersearch/PrisonerSearchResultDto.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.visitscheduler.dto.prisonersearch
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Prisoner information")
@@ -16,6 +17,7 @@ data class PrisonerSearchResultDto(
   @Schema(description = "Prisoner Category", example = "C")
   val category: String? = null,
 
+  @JsonProperty("lastPrisonId")
   @Schema(description = "Last Prison Code", example = "MDI", required = false)
   val lastPrisonCode: String? = null,
 )
