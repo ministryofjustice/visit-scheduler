a26b50998f9dcda8b8a0a53bbb8d645a2db8b1b1
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2024-07-19 16:11:25 +0100

    SQL script to remove incorrectly migrated visits (and applications) for PVI (#781)

---
+++ src/main/resources/db.scripts.mvp/remove_migrated_visits_for_PVI.sql
@@ -0,0 +1,54 @@
+BEGIN;
+
+    SET SCHEMA 'public';
+
+
+    CREATE TEMP TABLE tmp_visit_ids_to_be_deleted(
+        visit_id         int,
+        booking_reference text
+    );
+
+
+    CREATE TEMP TABLE tmp_application_ids_to_be_deleted(
+            application_id         int,
+            application_reference text
+    );
+
+    --selection criteria for applications to be deleted
+    INSERT INTO tmp_application_ids_to_be_deleted (application_id, application_reference) (
+        SELECT a.id, a.reference  from application a
+                                           join visit v on a.visit_id = v.id
+                                           join legacy_data ld on ld.visit_id = v.id
+                                           join prison p on p.id=v.prison_id
+        where p.code = 'PVI' AND ld.id is not null
+    );
+
+    DELETE FROM application_contact WHERE application_id in (select application_id FROM tmp_application_ids_to_be_deleted);
+    DELETE FROM application_support WHERE application_id in (select application_id FROM tmp_application_ids_to_be_deleted);
+    DELETE FROM application_visitor WHERE application_id in (select application_id FROM tmp_application_ids_to_be_deleted);
+    DELETE FROM application WHERE id in (select application_id FROM tmp_application_ids_to_be_deleted);
+    DELETE FROM event_audit ev where application_reference in (select application_reference FROM tmp_application_ids_to_be_deleted);
+
+
+    --selection criteria for visits to be deleted
+    INSERT INTO tmp_visit_ids_to_be_deleted (visit_id,booking_reference) (
+        SELECT v.id, v.reference  from visit v
+                              join legacy_data ld on ld.visit_id = v.id
+                              join prison p on p.id=v.prison_id
+        where p.code = 'PVI' AND ld.id is not null
+    );
+
+
+    DELETE FROM visit_contact WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_notes WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_support WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit_visitor WHERE visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM visit WHERE id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM legacy_data d where visit_id in (select visit_id FROM tmp_visit_ids_to_be_deleted);
+    DELETE FROM event_audit ev where booking_reference in (select booking_reference FROM tmp_visit_ids_to_be_deleted);
+
+    -- Drop temporary tables
+    DROP TABLE tmp_application_ids_to_be_deleted;
+    DROP TABLE tmp_visit_ids_to_be_deleted;
+
+END;
