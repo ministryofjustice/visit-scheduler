dc270c0f016b38d24cfe5de8404aadd7f140c57f
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-08-29 14:29:05 +0100

    Add actioned_by to prison_exclude_date and set existing values to NOT_KNOWN. Addd actionedBy to PrisonExcludeDateDto

--- src/main/resources/db/migration/V3_28__add_actioned_by_prison_exclude_date.sql
+++ src/main/resources/db/migration/V3_28__add_actioned_by_prison_exclude_date.sql
@@ -1,3 +1,3 @@
 ALTER TABLE prison_exclude_date ADD actioned_by VARCHAR(60);
-update prison_exclude_date set actioned_by = 'NOT_KNOWN';
+UPDATE prison_exclude_date set actioned_by = 'NOT_KNOWN' where actioned_by IS NULL;
 ALTER TABLE prison_exclude_date ALTER COLUMN actioned_by SET NOT NULL;
