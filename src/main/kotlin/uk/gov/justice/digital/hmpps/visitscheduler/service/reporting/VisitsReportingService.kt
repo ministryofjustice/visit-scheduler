19f4f4d4fa236cf79f77a6b3371edb92b50c391c
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-09-16 11:37:33 +0100

    Update to pass VisitReport

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/reporting/VisitsReportingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/reporting/VisitsReportingService.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.visitscheduler.service.reporting
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.VSIPReport
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.VSIPReport.VISIT_COUNTS_BY_DAY
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.reporting.SessionVisitCountsDto
 import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.VSIPReporting
 import uk.gov.justice.digital.hmpps.visitscheduler.repository.VSIPReportingRepository
@@ -17,27 +18,27 @@ class VisitsReportingService(
   fun getVisitCountsReportByDay(): Map<LocalDate, List<SessionVisitCountsDto>> {
     val sessionsReports = mutableMapOf<LocalDate, List<SessionVisitCountsDto>>()
 
-    val reportDate = getNextReportDate()
+    val reportDate = getNextReportDate(VISIT_COUNTS_BY_DAY)
     if (reportDate != null) {
       val maxReportDate = LocalDate.now()
       reportDate.datesUntil(maxReportDate).forEach { forDate ->
         val sessionReport = visitCountsByDateReportService.getVisitCountsReportForDate(forDate)
         sessionsReports[reportDate] = sessionReport
         visitCountsByDateReportService.sendTelemetryEvent(sessionReport)
-        updateLastRunReportDate(forDate)
+        updateLastRunReportDate(VISIT_COUNTS_BY_DAY, forDate)
       }
     } else {
-      LOG.info("No report date configured for {} report", VSIPReport.VISIT_COUNTS_BY_DAY)
+      LOG.info("No report date configured for {} report", VISIT_COUNTS_BY_DAY)
     }
     return sessionsReports
   }
 
-  private fun getNextReportDate(): LocalDate? {
-    val reportDetails = vsipReportingRepository.findById(VSIPReport.VISIT_COUNTS_BY_DAY)
+  private fun getNextReportDate(vsipReport: VSIPReport): LocalDate? {
+    val reportDetails = vsipReportingRepository.findById(vsipReport)
     return reportDetails.getOrNull()?.lastReportDate?.plusDays(1)
   }
 
-  private fun updateLastRunReportDate(reportDate: LocalDate) {
-    vsipReportingRepository.save(VSIPReporting(VSIPReport.VISIT_COUNTS_BY_DAY, reportDate))
+  private fun updateLastRunReportDate(vsipReport: VSIPReport, reportDate: LocalDate) {
+    vsipReportingRepository.save(VSIPReporting(vsipReport, reportDate))
   }
 }
