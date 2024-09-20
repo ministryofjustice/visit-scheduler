d9996b205aff798bb40043b7d867acca6ae9fcd8
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2024-09-20 09:22:33 +0100

    update deps (#840)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
   kotlin("plugin.spring") version "2.0.20"
   kotlin("plugin.jpa") version "2.0.20"
   idea
