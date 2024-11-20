e25f456dff71ea8482c96f4b4fbb80686f633033
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-11-20 09:23:56 +0000

    Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   idea
@@ -23,13 +23,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.1")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.10.0")
 
-  implementation("org.springframework.data:spring-data-commons:3.3.5")
+  implementation("org.springframework.data:spring-data-commons:3.4.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("org.springdoc:springdoc-openapi-starter-common:2.6.0")
@@ -46,7 +46,7 @@ dependencies {
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.23")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.testcontainers:localstack:1.20.3")
@@ -55,10 +55,10 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.jsonwebtoken:jjwt:0.12.6")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.20.1")
-  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.772")
-  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
-  testImplementation("org.junit-pioneer:junit-pioneer:2.2.0")
+  testImplementation("org.testcontainers:postgresql:1.20.3")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.778")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.1")
+  testImplementation("org.junit-pioneer:junit-pioneer:2.3.0")
 }
 
 java {
