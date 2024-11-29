2565bcfbc62d49f2e59e4dfb3cbb5c06463045fa
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-11-29 10:56:14 +0000

    Changes for create and callback events from Gov.uk Notify

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/builder/NotifyHistoryDtoBuilder.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.dto.builder
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.audit.NotifyHistoryDto
+import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.VisitNotifyHistory
+
+@Component
+class NotifyHistoryDtoBuilder {
+  fun build(notifyHistory: List<VisitNotifyHistory>?): List<NotifyHistoryDto> {
+    val notifyHistoryValues = mutableListOf<NotifyHistoryDto>()
+
+    if (!notifyHistory.isNullOrEmpty()) {
+      notifyHistory.groupBy({ it.notificationId }, { it }).forEach {
+        val notificationHistory = it.value.sortedByDescending { it.status.order }.first()
+        notifyHistoryValues.add(NotifyHistoryDto(notificationHistory))
+      }
+    }
+
+    return notifyHistoryValues.toList()
+  }
+}
