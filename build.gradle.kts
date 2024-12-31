b0c09363c6b75c1672e2beb5d6353cd53b18cd5c
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2024-12-31 09:52:50 +0000

    Update gradle-spring-boot version to 6.1.2 and keep kotlin plugin versions at 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.1"
-  kotlin("plugin.spring") version "2.1.0"
-  kotlin("plugin.jpa") version "2.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("plugin.jpa") version "2.0.21"
   idea
 }
 
