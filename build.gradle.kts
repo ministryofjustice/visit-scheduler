04cfad0b2bae06869800f803abb117116782bf01
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-01-27 09:13:28 +0000

    keep kotlin versions at 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
-  kotlin("plugin.spring") version "2.1.0"
-  kotlin("plugin.jpa") version "2.1.10"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("plugin.jpa") version "2.0.21"
   idea
 }
 
