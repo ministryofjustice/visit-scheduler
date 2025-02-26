c762fc358a297a3a6a9a35f9bb3fa5469f84d9b6
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-02-26 15:31:31 +0000

    Minor changes to flag visits report.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/enums/NotificationEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/enums/NotificationEventType.kt
@@ -2,13 +2,13 @@ package uk.gov.justice.digital.hmpps.visitscheduler.dto.enums
 
 enum class NotificationEventType(val reviewType: String, val description: String) {
   NON_ASSOCIATION_EVENT("Non-association", "non-association"),
-  PRISONER_RELEASED_EVENT("Prisoner-released", "prisoner-released"),
+  PRISONER_RELEASED_EVENT("Prisoner-released", "prisoner released"),
   PRISONER_RESTRICTION_CHANGE_EVENT("Prisoner-restriction-change", "prisoner restriction changed"),
   PRISON_VISITS_BLOCKED_FOR_DATE("Prison-visits-blocked-for-date", "visit date is blocked"),
   SESSION_VISITS_BLOCKED_FOR_DATE("Session-visits-blocked-for-date", "visit session is blocked"),
   PRISONER_RECEIVED_EVENT("Prisoner-received", "prisoner transferred"),
-  PRISONER_ALERTS_UPDATED_EVENT("Prisoner-alerts-updated", "prisoner alerts updated"),
-  PERSON_RESTRICTION_UPSERTED_EVENT("Person-restriction-upserted", "prisoner restriction added / updated"),
-  VISITOR_RESTRICTION_UPSERTED_EVENT("Visitor-restriction-upserted", "visitor restriction added / updated"),
+  PRISONER_ALERTS_UPDATED_EVENT("Prisoner-alerts-updated", "prisoner alerts changed"),
+  PERSON_RESTRICTION_UPSERTED_EVENT("Person-restriction-upserted", "visitor restriction changed"),
+  VISITOR_RESTRICTION_UPSERTED_EVENT("Visitor-restriction-upserted", "visitor restriction changed"),
   VISITOR_UNAPPROVED_EVENT("Visitor-unapproved", "visitor unapproved"),
 }
