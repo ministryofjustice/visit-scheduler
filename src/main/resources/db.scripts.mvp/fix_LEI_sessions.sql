5dd8a86c85eb8cf2cfe10af36527dd14f81f5e21
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-06-18 10:43:16 +0100

    Update fix_LEI_sessions.sql

--- src/main/resources/db.scripts.mvp/fix_LEI_sessions.sql
+++ src/main/resources/db.scripts.mvp/fix_LEI_sessions.sql
@@ -5,10 +5,10 @@ SET SCHEMA 'public';
 
 -- UPDATE session_slot set session_template_reference = 'NEW' where session_template_reference = 'OLD' and prison_id = (select id from prison where code = 'LEI');
 
-UPDATE session_slot set session_template_reference = 'dsx.cav.xyr' where session_template_reference = 'awx.ceg.xyx' and prison_id = (select id from prison where code = 'LEI');
+UPDATE session_slot set session_template_reference = 'awx.ceg.xyx' where session_template_reference = 'dsx.cav.xyr' and prison_id = (select id from prison where code = 'LEI');
 
-UPDATE session_slot set session_template_reference = 'nox.iws.dwv' where session_template_reference = 'sam.cvb.dam' and prison_id = (select id from prison where code = 'FMI');
+UPDATE session_slot set session_template_reference = 'sam.cvb.dam' where session_template_reference = 'nox.iws.dwv' and prison_id = (select id from prison where code = 'FMI');
 
-UPDATE session_slot set session_template_reference = 'gox.fnx.vrl' where session_template_reference = 'nam.ugo.eas' and prison_id = (select id from prison where code = 'FMI');
+UPDATE session_slot set session_template_reference = 'nam.ugo.eas' where session_template_reference = 'gox.fnx.vrl' and prison_id = (select id from prison where code = 'FMI');
 
 END;
