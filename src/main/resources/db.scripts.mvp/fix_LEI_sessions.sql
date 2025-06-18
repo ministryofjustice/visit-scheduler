b3e0a26451acfada7a3e27ad6ed40e42d459d40d
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-06-18 10:40:43 +0100

    updated with FMI sessions and adds missing LEI session

--- src/main/resources/db.scripts.mvp/fix_LEI_sessions.sql
+++ src/main/resources/db.scripts.mvp/fix_LEI_sessions.sql
@@ -1,4 +1,4 @@
--- Fix to update Leeds (LEI) from old session template to new session template
+-- Fix to update Leeds (LEI) and Feltham B (FMI) from old session template to new session template
 --| new code    | old code    | Day       |
 --| ----------- | ----------- | --------- |
 --| qvx.hxm.abs | wdx.fgs.spz | Monday    |
@@ -28,46 +28,11 @@ SET SCHEMA 'public';
 
 -- UPDATE session_slot set session_template_reference = 'NEW' where session_template_reference = 'OLD' and prison_id = (select id from prison where code = 'LEI');
 
-UPDATE session_slot set session_template_reference = 'qvx.hxm.abs' where session_template_reference = 'wdx.fgs.spz' and prison_id = (select id from prison where code = 'LEI');
+UPDATE session_slot set session_template_reference = 'dsx.cav.xyr' where session_template_reference = 'awx.ceg.xyx' and prison_id = (select id from prison where code = 'LEI');
 
-UPDATE session_slot set session_template_reference = 'rvx.iow.psg' where session_template_reference = 'asx.ibl.omx' and prison_id = (select id from prison where code = 'LEI');
+UPDATE session_slot set session_template_reference = 'nox.iws.dwv' where session_template_reference = 'sam.cvb.dam' and prison_id = (select id from prison where code = 'FMI');
 
-UPDATE session_slot set session_template_reference = 'bvx.hwe.ors' where session_template_reference = 'lsx.fyd.vse' and prison_id = (select id from prison where code = 'LEI');
+UPDATE session_slot set session_template_reference = 'gox.fnx.vrl' where session_template_reference = 'nam.ugo.eas' and prison_id = (select id from prison where code = 'FMI');
 
-UPDATE session_slot set session_template_reference = 'ovx.fwz.bgz' where session_template_reference = 'kdx.ier.nqx' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'dvx.hla.zay' where session_template_reference = 'ysx.imd.ozj' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'svx.ijo.nxe' where session_template_reference = 'xsx.ivm.adl' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'nvx.cso.erb' where session_template_reference = 'msx.faj.woy' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'pvx.uao.abd' where session_template_reference = 'qsx.fdq.gjr' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'evx.fjw.mvy' where session_template_reference = 'vsx.cjm.pjq' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'gvx.hpr.eqo' where session_template_reference = 'wsx.cyy.bmv' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'jvx.izx.oay' where session_template_reference = 'ksx.cab.bxs' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'vvx.fnl.ene' where session_template_reference = 'znx.fjq.ppq' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'wvx.csx.ale' where session_template_reference = 'rsx.hog.anq' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'kvx.csz.xjl' where session_template_reference = 'bsx.uqx.gjm' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'zwx.inl.dyr' where session_template_reference = 'osx.fne.ppy' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'lwx.csl.ngs' where session_template_reference = 'ssx.umv.zye' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'ywx.fya.nge' where session_template_reference = 'nsx.uwj.eel' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'xwx.fsr.msr' where session_template_reference = 'psx.fdb.pbg' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'mwx.crg.sgm' where session_template_reference = 'esx.uvb.sba' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'qwx.iwp.peg' where session_template_reference = 'gsx.hmr.jzs' and prison_id = (select id from prison where code = 'LEI');
-
-UPDATE session_slot set session_template_reference = 'rwx.isj.mrx' where session_template_reference = 'jsx.inp.ngn' and prison_id = (select id from prison where code = 'LEI');
 
 END;
