6deb4a69b2a9a2809b7c131bbb685493347c1ec9
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2025-07-17 11:36:55 +0100

    VB-4953 / VB-5779 - New endpoint to approve visit request (Initial work) (#1065)
    * VB-4953 / VB-5779 - New endpoint to approve visit request
    
    Initial endpoint implementation. Simply approves the request, does not
    raise any events or do additional saving. This will come in later commits.
    
    * add next steps as TODO
    
    * Update to return required info (visit reference and prisoner name)
    
    * more comments
    
    * add more notes
    
    * Move prisoner detail retrival into orchestration after PR comments
    
    * handle visit not existing route

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsService.kt
@@ -4,8 +4,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitDto
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitRequestSummaryDto
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.builder.VisitDtoBuilder
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.EventAuditType
+import uk.gov.justice.digital.hmpps.visitscheduler.exception.ItemNotFoundException
 import uk.gov.justice.digital.hmpps.visitscheduler.repository.VisitRepository
 
 @Service
@@ -14,6 +17,7 @@ class VisitRequestsService(
   private val visitRepository: VisitRepository,
   private val prisonerService: PrisonerService,
   private val visitEventAuditService: VisitEventAuditService,
+  private val visitDtoBuilder: VisitDtoBuilder,
 ) {
   companion object {
     val LOG: Logger = LoggerFactory.getLogger(this::class.java)
@@ -56,5 +60,14 @@ class VisitRequestsService(
     }
 
     return visitRequestSummaryList.sortedBy { it.visitDate }
+  }
+
+  fun approveVisitRequestByReference(visitReference: String): VisitDto {
+    val success = visitRepository.approveVisitRequestForPrisonByReference(visitReference) > 0
+    if (!success) {
+      throw ItemNotFoundException("No visit request found for reference $visitReference")
+    }
+
+    return visitDtoBuilder.build(visitRepository.findByReference(visitReference)!!)
   }
 }
