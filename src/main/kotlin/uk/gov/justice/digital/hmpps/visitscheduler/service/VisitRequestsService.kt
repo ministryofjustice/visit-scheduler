59abfc478fc61aaa65d48d47293179f692903b8d
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-07-17 11:17:15 +0100

    handle visit not existing route

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsService.kt
@@ -8,6 +8,7 @@ import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitDto
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitRequestSummaryDto
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.builder.VisitDtoBuilder
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.EventAuditType
+import uk.gov.justice.digital.hmpps.visitscheduler.exception.ItemNotFoundException
 import uk.gov.justice.digital.hmpps.visitscheduler.repository.VisitRepository
 
 @Service
@@ -62,14 +63,11 @@ class VisitRequestsService(
   }
 
   fun approveVisitRequestByReference(visitReference: String): VisitDto {
-    visitRepository.approveVisitRequestForPrisonByReference(visitReference)
+    val success = visitRepository.approveVisitRequestForPrisonByReference(visitReference) > 0
+    if (!success) {
+      throw ItemNotFoundException("No visit request found for reference $visitReference")
+    }
 
     return visitDtoBuilder.build(visitRepository.findByReference(visitReference)!!)
-    // TODO: VB-4953 (Staff approves visit request):
-    //  - Add validation check for double approval / rejection (if subStatus != REQUESTED -> 400 exception) (VB-5793)
-    //  - Add new event for staff approving visit to Event Audit table (VB-5780)
-    //  - Add logic to find the visit in the visit_notification_events table and un-flag any entries (VB-5787)
-    //  - Add logic to raise to application insights (VB-5792)
-    //  - Add new domain event and raise it (after of transaction completion) for notification service to consume and send comms (VB-5791)
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/integration/visit/ApproveVisitRequestTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/integration/visit/ApproveVisitRequestTest.kt
@@ -4,26 +4,22 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.transaction.annotation.Propagation.SUPPORTS
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.visitscheduler.controller.VISIT_REQUESTS_APPROVE_VISIT_BY_REFERENCE_PATH
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitDto
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.EventAuditType
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.VisitRestriction
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.VisitStatus.BOOKED
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.VisitSubStatus
 import uk.gov.justice.digital.hmpps.visitscheduler.helper.callApproveVisitRequest
 import uk.gov.justice.digital.hmpps.visitscheduler.integration.IntegrationTestBase
-import uk.gov.justice.digital.hmpps.visitscheduler.repository.TestVisitRepository
-import java.time.LocalDate
 
 @Transactional(propagation = SUPPORTS)
 @DisplayName("Get $VISIT_REQUESTS_APPROVE_VISIT_BY_REFERENCE_PATH")
 class ApproveVisitRequestTest : IntegrationTestBase() {
-
-  @Autowired
-  private lateinit var testVisitRepository: TestVisitRepository
 
   private lateinit var roleVisitSchedulerHttpHeaders: (HttpHeaders) -> Unit
 
@@ -38,15 +34,8 @@ class ApproveVisitRequestTest : IntegrationTestBase() {
   @Test
   fun `when approve visit requests endpoint is called, then visit is successfully approved`() {
     // Given
-    val visitPrimary = visitEntityHelper.create(
-      prisonerId = primaryPrisonerId,
-      slotDate = LocalDate.now().plusDays(1),
-      visitStatus = BOOKED,
-      visitSubStatus = VisitSubStatus.REQUESTED,
-      prisonCode = sessionTemplateDefault.prison.code,
-      sessionTemplate = sessionTemplateDefault,
-    )
-    eventAuditEntityHelper.create(visitPrimary)
+    val visitPrimary = createApplicationAndVisit(sessionTemplate = sessionTemplateDefault, visitRestriction = VisitRestriction.OPEN, visitStatus = BOOKED, visitSubStatus = VisitSubStatus.REQUESTED)
+    eventAuditEntityHelper.create(visitPrimary, type = EventAuditType.REQUESTED_VISIT)
 
     // When
     val responseSpec = callApproveVisitRequest(webTestClient, visitPrimary.reference, roleVisitSchedulerHttpHeaders)
@@ -56,6 +45,17 @@ class ApproveVisitRequestTest : IntegrationTestBase() {
     val approvedVisit = getApproveVisitRequestResponse(responseSpec)
     assertThat(approvedVisit.reference).isEqualTo(visitPrimary.reference)
     assertThat(approvedVisit.visitSubStatus).isEqualTo(VisitSubStatus.APPROVED)
+  }
+
+  @Test
+  fun `when approve visit requests endpoint is called, but no visit exists then 404 is thrown`() {
+    // Given no visit exists on the visit-scheduler
+
+    // When
+    val responseSpec = callApproveVisitRequest(webTestClient, "no_visit", roleVisitSchedulerHttpHeaders)
+
+    // Then
+    responseSpec.expectStatus().isNotFound
   }
 
   private fun getApproveVisitRequestResponse(responseSpec: WebTestClient.ResponseSpec): VisitDto = objectMapper.readValue(responseSpec.expectBody().returnResult().responseBody, VisitDto::class.java)
