433201b06aa091da046c40f749d592eb75716824
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2025-07-29 12:51:00 +0100

    Revert "Add indexes for most common DB queries for visit requests (#1091)"
    This reverts commit ce83f059e18aa8bf8bdf8a36f363f2bb3035544c.

--- src/main/resources/db/migration/V4_1__add_indexes_for_slow_public_visit_queries.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX idx_event_audit_booked_visit ON event_audit (booking_reference, actioned_by_id) WHERE type = 'BOOKED_VISIT';
-
-CREATE INDEX idx_event_audit_reserved_visit ON event_audit (booking_reference, actioned_by_id) WHERE type = 'RESERVED_VISIT';
-
-CREATE INDEX idx_visit_user_status_slot ON visit (user_type, visit_status, session_slot_id);
