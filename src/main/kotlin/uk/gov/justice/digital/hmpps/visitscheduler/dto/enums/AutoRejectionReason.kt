113e1cf57d7cb0afff3fc88fa12186651e82e118
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2025-09-04 09:03:15 +0100

    VB-5437 - Prisoner released auto rejects visit requests (#1123)
    * VB-5437 - Prisoner released auto rejects visit requests
    
    When a prisoner released event is consumed, booked visits will be
    flagged and request visits will be auto rejected
    
    * add rejection text to event audit for better paper trail
    
    * Improve the auto rejection logging

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/enums/AutoRejectionReason.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.visitscheduler.dto.enums
+
+enum class AutoRejectionReason(
+  val description: String,
+) {
+  MINIMUM_BOOKING_WINDOW_REACHED("Auto rejected by minimum booking window system cron"),
+  PRISONER_RELEASED("Auto rejected by prisoner released event"),
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsApprovalRejectionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/service/VisitRequestsApprovalRejectionService.kt
@@ -9,7 +9,6 @@ import uk.gov.justice.digital.hmpps.visitscheduler.dto.ApproveRejectionVisitRequ
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.VisitRequestApprovalRejectionResponseDto
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.builder.VisitDtoBuilder
 import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.UnFlagEventReason
-import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.Visit
 import uk.gov.justice.digital.hmpps.visitscheduler.repository.VisitRepository
 
 @Transactional
@@ -53,13 +52,13 @@ class VisitRequestsApprovalRejectionService(
     return VisitRequestApprovalRejectionResponseDto(actionedVisitDto, eventAuditDto)
   }
 
-  fun autoRejectRequestVisitsAtMinimumBookingWindow(visitRequest: Visit): VisitRequestApprovalRejectionResponseDto {
+  fun autoRejectRequestByVisitReference(visitReference: String, rejectionText: String): VisitRequestApprovalRejectionResponseDto {
     LOG.info("Entered VisitRequestsApprovalRejectionService - autoRejectRequestVisitsAtMinimumBookingWindow")
 
-    visitRepository.autoRejectVisitRequestByReference(visitRequest.reference)
+    visitRepository.autoRejectVisitRequestByReference(visitReference)
 
-    val actionedVisitDto = visitDtoBuilder.build(visitRepository.findByReference(visitRequest.reference)!!)
-    val eventAuditDto = visitEventAuditService.saveVisitRequestAutoRejectedEventAudit(actionedVisitDto)
+    val actionedVisitDto = visitDtoBuilder.build(visitRepository.findByReference(visitReference)!!)
+    val eventAuditDto = visitEventAuditService.saveVisitRequestAutoRejectedEventAudit(actionedVisitDto, rejectionText)
 
     visitNotificationEventService.deleteVisitAndPairedNotificationEvents(actionedVisitDto.reference, UnFlagEventReason.VISIT_REQUEST_AUTO_REJECTED)
 
