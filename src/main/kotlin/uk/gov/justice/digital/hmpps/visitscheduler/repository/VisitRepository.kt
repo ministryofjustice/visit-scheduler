8387c8f4188aeedb9b4fd998024c0ac83dbb3873
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-04-21 12:29:24 +0100

    Updates.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/repository/VisitRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/repository/VisitRepository.kt
@@ -454,6 +454,7 @@ interface VisitRepository :
       "INNER JOIN session_slot sl ON sl.id = v.session_slot_id " +
       "WHERE v.visit_status = 'BOOKED' " +
       "AND v.visit_sub_status = 'REQUESTED' " +
+      "AND sl.slot_start >= NOW() " +
       "AND pc.user_type = 'PUBLIC' " +
       "AND sl.slot_date <= (CURRENT_DATE + pc.policy_notice_days_min + 1)", // +1 here to handle 0 day booking window (E.g. Monday rejects Sunday, NOT Monday).
     nativeQuery = true,
