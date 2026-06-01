3e3af7027d5707cbd1eda75d2057d4d23271fe0a
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2026-06-01 12:44:52 +0100

    VB-6593 - Add language preference to main contact (#1252)
    * VB-6593 - Add language preference to main contact
    
    This field will be used to determine what language the main contact
    would like their communications (email / sms) in.
    
    * Update tests and other areas of code to use language preference
    
    * fix var name
    
    * use update dto var to allow updating language
    
    * remove unused imports
    
    * add var to toString override

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/ContactDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitscheduler/dto/ContactDto.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.visitscheduler.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import uk.gov.justice.digital.hmpps.visitscheduler.dto.enums.notify.LanguagePreference
 import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.VisitContact
 import uk.gov.justice.digital.hmpps.visitscheduler.model.entity.application.ApplicationContact
 import uk.gov.justice.digital.hmpps.visitscheduler.validation.NullableNotBlank
@@ -17,16 +18,20 @@ data class ContactDto(
   @param:Schema(description = "Contact Email Address", example = "email@example.com", required = false)
   @field:NullableNotBlank
   val email: String?,
+  @param:Schema(description = "Contact language preference", example = "CY", defaultValue = "EN", required = false)
+  val languagePreference: LanguagePreference = LanguagePreference.EN,
 ) {
   constructor(entity: VisitContact) : this(
     name = entity.name,
     telephone = entity.telephone,
     email = entity.email,
+    languagePreference = entity.languagePreference,
   )
 
   constructor(entity: ApplicationContact) : this(
     name = entity.name,
     telephone = entity.telephone,
     email = entity.email,
+    languagePreference = entity.languagePreference,
   )
 }
---
+++ src/main/resources/db/migration/V4_5__add_language_preference_to_visit_contact_and_application_contact.sql
@@ -0,0 +1,5 @@
+ALTER TABLE application_contact
+    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'EN';
+
+ALTER TABLE visit_contact
+    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'EN';
