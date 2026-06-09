6b5274422265cc74fa92caf63fcf82af796bc5b3
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-06-09 11:54:07 +0100

    Avoid mass update and instead set to lowercase by default

--- src/main/resources/db/migration/V4_5__add_language_preference_to_visit_contact_and_application_contact.sql
+++ src/main/resources/db/migration/V4_5__add_language_preference_to_visit_contact_and_application_contact.sql
@@ -1,5 +1,5 @@
 ALTER TABLE application_contact
-    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'EN';
+    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'en';
 
 ALTER TABLE visit_contact
-    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'EN';
+    ADD COLUMN language_preference VARCHAR(2) NOT NULL DEFAULT 'en';
