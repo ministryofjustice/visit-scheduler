8523cfe827d2ee354a74804abafb0048882e2571
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-09-09 12:51:41 +0100

    Revert kotlin plugins from 2.4.20 to 2.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.8"
-  kotlin("plugin.spring") version "2.4.20"
-  kotlin("plugin.jpa") version "2.4.20"
+  kotlin("plugin.spring") version "2.4.10"
+  kotlin("plugin.jpa") version "2.4.10"
   idea
   id("org.owasp.dependencycheck") version "13.0.0"
 }
